fb79379a127d32eab6c230c1ff8eb36fc7f1ebeb
Jung-hwan <ahnmo@pagez.kr>
2018-06-19 15:52:04 +0900

    Update blacklist feature

--- js/background.js
+++ js/background.js
@@ -3,6 +3,49 @@ chrome.browserAction.onClicked.addListener(onClickListner);
 chrome.notifications.onButtonClicked.addListener(onButtenClickedListner);
 chrome.runtime.onMessage.addListener(onMessageListener);
 
+let domains = [];
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  domains[tabId] = getDomain(tab.url);
+});
+
+chrome.tabs.onRemoved.addListener(tabId => {
+    delete domains[tabId];
+});
+
+// TODO: implementation
+function isWhitelistByUser(domain) {
+  return false;
+}
+
+// TODO: implementation
+function getUserBlacklist() {
+  return [];
+}
+
+
+function onBeforeRequesHandler(details) {
+  if (isWhitelistByUser(domains[details.tabId])) {
+    return {cancel: false};
+  }
+  return {cancel: true};
+}
+
+function registerOnBeforeReuqest(data) {
+  if (chrome.webRequest.onBeforeRequest.hasListener(onBeforeRequesHandler)) {
+		chrome.webRequest.onBeforeRequest.removeListener(onBeforeRequesHandler);
+	}
+  // if (config.disabled) return;
+
+  var serv_blacklist = data;//data.split('\n');
+  var user_blacklist = getUserBlacklist();
+  var merged = serv_blacklist.concat(user_blacklist);
+
+  chrome.webRequest.onBeforeRequest.addListener(onBeforeRequesHandler, {
+      urls: merged
+  }, ['blocking']);
+}
+
 function onClickListner(activeTab) {
     chrome.tabs.create({ url: chrome.extension.getURL('../index.html') });
 }
@@ -44,3 +87,23 @@ function showNotification(tabID) {
 function sendMessage(tabID, text) {
 	chrome.tabs.sendMessage(tabID, {message: text});
 }
+
+(function() {
+  fetch('https://raw.githubusercontent.com/AhnMo/Mining-Lock/master/blacklist.json')
+  .then(function(response) {
+    console.log(response.ok);
+    if (!response.ok) {
+      throw new Error('Network error');
+    }
+    return response.json();
+  })
+  .then(registerOnBeforeReuqest)
+  .catch(function(error) {
+    console.log(error.message);
+
+    fetch(chrome.extension.getURL('blacklist.json'))
+    .then(function(response) {
+      return response.json()
+    }).then(registerOnBeforeReuqest)
+  });
+})();
